1be9f116fb06bff76c1cce3eb780baf0d2f5c191
zhichuanxiu <719118794@qq.com>
2015-10-24 09:23:54 +0800

    update remove_control_char

--- external_scripts/remove_control_char.py
+++ external_scripts/remove_control_char.py
@@ -1,4 +1,5 @@
 #!/bin/env python
+# coding: utf-8
 
 
 def remove_control_char(filename):
@@ -8,13 +9,14 @@ def remove_control_char(filename):
 
 	control_char=re.compile(r"""
 				\x1b[ #%()*+\-.\/]. |
-				\r |
-				(?:\x1b\[|\x9b) [ -?]* [@-~] |
-				(?:\x1b\]|\x9d) .*? (?:\x1b\\|[\a\x9c]) |
-				(?:\x1b[P^_]|[\x90\x9e\x9f]) .*? (?:\x1b\\|\x9c) |
-				\x1b. |
-				[\x80-\x9f] |
-							""",re.X)
+				\r |                                               #匹配 回车符(CR) 
+				(?:\x1b\[|\x9b) [ -?]* [@-~] |                     #匹配 控制顺序描述符(CSI)... Cmd
+				(?:\x1b\]|\x9d) .*? (?:\x1b\\|[\a\x9c]) |          #匹配 操作系统指令(OSC)...终止符或振铃符(ST|BEL)
+				(?:\x1b[P^_]|[\x90\x9e\x9f]) .*? (?:\x1b\\|\x9c) | #匹配 设备控制串或私讯或应用程序命令(DCS|PM|APC)...终止符(ST)
+				\x1b. |                                            #匹配 转义过后的字符
+				[\x80-\x9f] |                                      #匹配 所有控制字符 
+				""",
+                                re.X)
 	backspace=re.compile(r"[^\b][\b]")
 
 	for line in file_line_list:
